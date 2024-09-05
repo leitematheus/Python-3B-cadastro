8d2397f09e23aea1e0a6463531c1e3cfbf7606a7
Matheus Henrique Leite da Silva <110172122+leitematheus@users.noreply.github.com>
2024-09-05 19:38:19 -0300

    05/09

--- app.py
+++ app.py
@@ -9,23 +9,43 @@ def cadastro_clientes():
     cliente = {
         'nome': nome,
         'email': email,
-        'telefone': telefone
+        'telefone': telefone,
+        'ativo': True
     }
 
     clientes.append(cliente)
     print('Cliente cadastrado com sucesso!')
 
 def listar_clientes():
+# Aqui você pode adicionar o código para listar clientes
     print('Listar Clientes')
-    # Aqui você pode adicionar o código para listar clientes
+    if clientes:
+        for idx, cliente in enumerate(clientes):
+            status = 'Ativo' if cliente['ativo'] else 'Inativo'
+            print(f'{idx + 1}. Nome: {cliente['nome']}, Email: {cliente['email']}, Telefone : {cliente['telefone']}, Status: {status}')
+    else:
+        print('Nenhum cliente cadastrado.\n')
 
 def ativar_cliente():
+# Aqui você pode adicionar o código para ativar ou desativar clientes
     print('Ativar/Desativar Cliente')
-    # Aqui você pode adicionar o código para ativar ou desativar clientes
+    listar_clientes()
+    if clientes:
+        try:
+            cliente_id = int(input('Escolha o número do cliente para ativar/desativar: '))
+            if 1 <= cliente_id <= len(clientes):
+                cliente = clientes[cliente_id - 1]
+                cliente['ativo'] = not cliente['ativo']
+                print(f"Cliente {cliente['nome']} agora está {'Ativo' if cliente['ativo'] else 'Inativo'}.\n")
+            else:
+                print('Cliente não encontrado!\n')
+        except ValueError:
+            print('Entrada inválida! Por favor, insira um número válido.\n')
 
 def sair_aplicacao():
+     # Aqui você pode adicionar o código para encerrar a aplicação
     print('Saindo da Aplicação')
-    # Aqui você pode adicionar o código para encerrar a aplicação
+    exit()
 
 def exibir_menu():
     print('''
